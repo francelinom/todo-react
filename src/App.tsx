142369777160893b83d89641f4d410a048f74736
Francelino <ext.francelino.silva@motz.com.br>
2025-09-18 16:36:10 -0300

    feat: add component icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import ChackIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
 import Text from "./components/Text";
 
 export default function App() {
 
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <Text variant="body-md-bold" className="text-pink-base">
-        Olá Mundo - ToDo List
-      </Text>
+    <div className="grid gap-1">
+      <div className="flex flex-col gap-4 p-4">
+        <Text variant="body-md-bold" className="text-pink-base">
+          Olá Mundo - ToDo List
+        </Text>
+
+        <Text variant="body-md" className="text-green-dark">
+          Olá Mundo - ToDo List
+        </Text>
+
+        <Text variant="body-sm-bold" className="text-gray-400">
+          Olá Mundo - ToDo List
+        </Text>
+
+        <Text>
+          Olá Mundo - ToDo List
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base"/>
+        <Icon svg={ChackIcon} className="fill-green-base"/>
+        <Icon svg={PencilIcon} className="fill-green-dark"/>
+        <Icon svg={PlusIcon} className="fill-pink-dark"/>
+        <Icon svg={SpinnerIcon} animate />
+      </div>
     </div>
+
+
   )
 }
